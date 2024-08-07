6ed062f0c64a28501176f1e55988620d3989aee2
muhammed ajmal <muhammedajmalcc6424094@gmail.com>
2024-08-07 17:10:17 +0530

    admin product route clear some bugs

--- controller/companyController.js
+++ controller/companyController.js
@@ -4,15 +4,18 @@ module.exports={
 
 agenyformPost: async(req,res) => {
     try {
-      req.body.companyImage = req.file.filename;
-      console.log(req.body.companypickupImage,'this is image');
-      const data = new Company(req.body)
-      await data.save()
-console.log(data,'data is saved ');
-        
+      console.log('here');
+
       } catch (error) {
         console.log(error, "error in  pickup post");
       }
     },
 }
 
+      
+      // req.body.companyImage = req.file.filename;
+      // console.log(req.body.companypickupImage,'this is image');
+      // const data = new Company(req.body)
+      // await data.save()
+// console.log(data,'data is saved ');
+        
